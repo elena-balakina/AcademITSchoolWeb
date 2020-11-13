5ab489fc0598f5cef2e204189e83c653b7815f68
vera-ПК\vera <e.p.balakina@mail.ru>
2020-11-13 21:25:40 +0700

    Работа с массивами v2

--- Работа с массивами/script.js
+++ Работа с массивами/script.js
@@ -1,41 +1,46 @@
-var array = [1, 3, 10, 8, 5, -4, 18, 24, 0, -7, 2, 22, 93]
+(function () {
+    "use strict";
 
-console.log("Отсортированный по убыванию массив: " + sortByDescending(array));
-console.log("Первые 5 элементов массива: " + array.slice(0, 6));
-console.log("Последние 5 элементов массива: " + array.slice(array.length - 5));
-console.log("Сумма четных элементов массива: " + getEvenNumbersSum(array));
+    var array = [1, 3, 10, 8, 5, -4, 18, 24, 0, -7, 2, 22, 93];
 
-var array1to100 = getArray(1, 100);
-console.log(array1to100);
-console.log("Массив квадратов четных чисел данного массива: " + getEvenNumbersSquaresArray(array1to100));
+    console.log("Отсортированный по убыванию массив: " + sortByDescending(array));
+    console.log("Первые 5 элементов массива: " + array.slice(0, 5));
+    console.log("Последние 5 элементов массива: " + array.slice(array.length - 5));
+    console.log("Сумма четных элементов массива: " + getEvenNumbersSum(array));
 
-function sortByDescending(array) {
-    return array.sort(function (e1, e2) {
-        return e2 - e1;
-    });
-}
+    var array1to100 = getArray(1, 100);
+    console.log(array1to100);
+    console.log("Массив квадратов четных чисел данного массива: " + getEvenNumbersSquaresArray(array1to100));
 
-function getEvenNumbersSum(array) {
-    return array.filter(function (e) {
-        return e % 2 === 0;
-    }).reduce(function (sum, current) {
-        return sum + current;
-    }, 0);
-}
-
-function getArray(from, to) {
-    var array = [];
-
-    for (var i = from; i <= to; i++) {
-        array.push(i);
+    function sortByDescending(array) {
+        return array.sort(function (e1, e2) {
+            return e2 - e1;
+        });
     }
-    return array;
-}
 
-function getEvenNumbersSquaresArray(array) {
-    return array.filter(function (e) {
-        return e % 2 === 0;
-    }).map(function (e) {
-        return e * e;
-    });
-}
+    function getEvenNumbersSum(array) {
+        return array.filter(function (e) {
+            return e % 2 === 0;
+        }).reduce(function (sum, current) {
+            return sum + current;
+        }, 0);
+    }
+
+    function getArray(from, to) {
+        var array = [];
+
+        for (var i = from; i <= to; i++) {
+            array.push(i);
+        }
+
+        return array;
+    }
+
+    function getEvenNumbersSquaresArray(array) {
+        return array.filter(function (e) {
+            return e % 2 === 0;
+        }).map(function (e) {
+            return e * e;
+        });
+    }
+})();
