ecaa5045247ff257ebbdb5d5d8edee8995eda8f0
vera-ПК\vera <e.p.balakina@mail.ru>
2020-11-16 18:06:15 +0700

    Temperature converter v1.1

--- Temperature converter/script.js
+++ Temperature converter/script.js
@@ -1,34 +1,42 @@
-(function () {
-    "use strict";
-    document.addEventListener("DOMContentLoaded", function ready() {
-        function convertToKelvin(celsius) {
-            return (celsius + 273.15).toFixed(2);
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function ready() {
+    function convertToKelvin(celsius) {
+        return (celsius + 273.15).toFixed(2);
+    }
+
+    function convertToFahrenheit(celsius) {
+        return (celsius * 1.8 + 32).toFixed(2);
+    }
+
+    function convertTemperatures() {
+        var celsius = document.getElementById('celsius');
+        var celsiusTemperature = Number(celsius.value);
+
+        var kelvin = document.getElementById('kelvin');
+        var fahrenheit = document.getElementById('fahrenheit');
+
+        if (celsiusTemperature === 0) {
+            alert("Введите число")
+        } else if (isNaN(celsiusTemperature)) {
+            alert("Введите число");
+        } else {
+            kelvin.textContent = convertToKelvin(celsiusTemperature);
+            fahrenheit.textContent = convertToFahrenheit(celsiusTemperature);
         }
 
-        function convertToFahrenheit(celsius) {
-            return (celsius * 1.8 + 32).toFixed(2);
-        }
+        celsius.value = "";
+    }
 
-        function convertTemperatures() {
-            var celsius = document.getElementsByName('celsius');
-            var celsiusTemperature = Number(celsius.value);
-
-            var kelvin = document.getElementById('kelvin');
-            var fahrenheit = document.getElementById('fahrenheit');
-
-            if (celsiusTemperature === 0) {
-                alert("Empty")
-            } else if (isNaN(celsiusTemperature)) {
-                alert("Введите число");
-            } else {
-                kelvin.textContent = convertToKelvin(celsiusTemperature);
-                fahrenheit.textContent = convertToFahrenheit(celsiusTemperature);
-            }
-        }
-
-        var convertButton = document.getElementById('convert-button');
-        convertButton.addEventListener("click", function () {
-            convertTemperatures();
-        });
+    var convertButton = document.getElementById('convert-button');
+    convertButton.addEventListener("click", function () {
+        convertTemperatures();
     });
-})();
+
+    var celsius = document.getElementById('celsius');
+    celsius.addEventListener("keydown", function (e) {
+        if (e.key === "Enter") {
+            convertTemperatures();
+        }
+    });
+});
