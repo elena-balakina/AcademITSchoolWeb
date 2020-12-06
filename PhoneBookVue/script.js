5e71a29e0130f08f13437c63b2d4e8fe9dc16768
vera-ПК\vera <e.p.balakina@mail.ru>
2020-12-06 17:40:23 +0700

    PhoneBookVue v1

--- PhoneBookVue/script.js
+++ PhoneBookVue/script.js
@@ -6,14 +6,43 @@ new Vue({
         newFirstName: "",
         newLastName: "",
         newPhoneNumber: "",
-        newId: 1
+        newId: 1,
+        errors: []
     },
 
     methods: {
+        checkFields: function () {
+            this.errors = [];
+
+            if (this.newFirstName.length === 0) {
+                this.errors.push('First name can not be empty');
+            }
+
+            if (this.newLastName.length === 0) {
+                this.errors.push('Last name can not be empty');
+            }
+
+            if (this.newPhoneNumber.length === 0) {
+                this.errors.push('Phone number can not be empty');
+            } else {
+                var phoneNumberInt = parseInt(this.newPhoneNumber.toString(), 10);
+
+                if (isNaN(phoneNumberInt)) {
+                    this.errors.push('Phone number must contain only digits');
+                }
+            }
+
+            return !this.errors.length;
+        },
+
         addContact: function () {
+            if (!this.checkFields()) {
+                return
+            }
+
             var firstName = this.newFirstName;
-            var lastName = this.lastName;
-            var phoneNumber = this.phoneNumber;
+            var lastName = this.newLastName;
+            var phoneNumber = this.newPhoneNumber;
 
             this.items.push({
                 id: this.newId,
@@ -26,6 +55,12 @@ new Vue({
             this.newLastName = "";
             this.newPhoneNumber = "";
             this.newId++;
+        },
+
+        deleteContact: function (item) {
+            this.items = this.items.filter(function (x) {
+                return x !== item;
+            });
         }
     }
 });
