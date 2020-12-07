94c01c365965301ed555909a710f5cb767fdcaf6
vera-ПК\vera <e.p.balakina@mail.ru>
2020-12-07 20:12:26 +0700

    Temperature converter v3

--- Temperature converter/script.js
+++ Temperature converter/script.js
@@ -2,13 +2,12 @@
 
 document.addEventListener("DOMContentLoaded", function ready() {
     var celsiusInputField = document.getElementById("celsius");
-    var kelvinInputField = document.getElementById("kelvin");
-    var fahrenheitInputField = document.getElementById("fahrenheit");
+    var kelvinOutputField = document.getElementById("kelvin");
+    var fahrenheitOutputField = document.getElementById("fahrenheit");
     var convertButton = document.getElementById("convert-button");
 
     convertButton.addEventListener("click", function () {
-        var celsiusTemperature = Number(celsiusInputField.value);
-        convertTemperatures(celsiusTemperature);
+        convertTemperatures();
     });
 
     celsiusInputField.addEventListener("keydown", function (e) {
@@ -26,12 +25,16 @@ document.addEventListener("DOMContentLoaded", function ready() {
         return (celsiusTemperature * 1.8 + 32).toFixed(2);
     }
 
-    function convertTemperatures(celsiusTemperature) {
-        if (celsiusInputField.value === "" || isNaN(celsiusTemperature)) {
+    function convertTemperatures() {
+        var celsiusTemperature = Number(celsiusInputField.value);
+
+        if (celsiusInputField.value.trim() === "" || isNaN(celsiusTemperature)) {
+            kelvinOutputField.textContent = "";
+            fahrenheitOutputField.textContent = "";
             alert("Введите число");
         } else {
-            kelvinInputField.textContent = convertToKelvin(celsiusTemperature);
-            fahrenheitInputField.textContent = convertToFahrenheit(celsiusTemperature);
+            kelvinOutputField.textContent = convertToKelvin(celsiusTemperature);
+            fahrenheitOutputField.textContent = convertToFahrenheit(celsiusTemperature);
         }
     }
 });
